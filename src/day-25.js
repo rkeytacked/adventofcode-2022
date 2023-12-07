2ce08a637688077f6dbcdbc65b9a2de4b76994fd
Peter Liske <peter.liske@gmx.net>
2023-12-07 16:35:05 +0100

    shorten code

--- src/day-25.js
+++ src/day-25.js
@@ -1,68 +1,32 @@
 #!/usr/bin/env node
-const {
-    log,
-    associate,
-    readCharArrays,
-    readLines,
-    readSingletonMaps,
-    split,
-    sum,
-    toNumber,
-    min,
-    max,
-    sort,
-    prod,
-    median
-} = require("./common");
+const {log, readLines, sum} = require("./common");
 
 /* * * * * * * *
  * * DAY  25 * *
  * * * * * * * */
 
 const input = readLines('../inputs/25.txt');
-const snafu = {
-    "2": 2,
-    "1": 1,
-    "0": 0,
-    "-": -1,
-    "=": -2,
-};
 
 /* * * * * * * *
  * * Part #1 * *
  * * * * * * * */
 
+const snafu = {
+    "=": -2,
+    "-": -1,
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": '=',
+    "4": '-',
+};
+
 function parseSnafu(nr) {
-    let value = 0;
-    for (let char of nr) {
-        value = 5 * value + snafu[char];
-    }
-    return value;
+    return [...nr].reduce((value, char) => 5 * value + snafu[char], 0);
 }
 
 function toSnafu(nr) {
-    if (!nr) return '0';
-    let string = '';
-    while (nr > 0) {
-        let remainder = nr % 5;
-        switch (remainder) {
-            case 0:
-            case 1:
-            case 2:
-                string = remainder + string;
-                nr = (nr - remainder) / 5;
-                break;
-            case 3:
-                string = "=" + string;
-                nr = (nr + 2) / 5;
-                break;
-            case 4:
-                string = "-" + string;
-                nr = (nr + 1) / 5;
-                break;
-        }
-    }
-    return string;
+    return nr ? toSnafu(Math.floor((nr + 2) / 5)) + snafu[nr % 5] : '';
 }
 
 log('solution #1:', toSnafu(sum(input.map(parseSnafu))));
